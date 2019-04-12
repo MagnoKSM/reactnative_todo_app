7a79196ebae4c8e2d955521cf589271f46c44059
MagnoKsm <magno.bds@gmail.com>
2019-04-12 14:40:25 -0300

    Add Project

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader.jsx'
+
+export default props => (
+    <div>
+        <PageHeader name='About' small='us'>
+                </PageHeader>
+    
+        
+    </div>
+)
